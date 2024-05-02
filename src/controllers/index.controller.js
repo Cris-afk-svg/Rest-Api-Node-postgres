8dc152e86c26bc17a83777dff5b236dc02fa1cf1
CrissCross <127163310+Cris-afk-svg@users.noreply.github.com>
2024-05-02 04:00:25 -0600

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
+    host: 'https://rest-api-node-postgres.onrender.com/',
     user: 'postgres',
     password: '12345',
     database: 'firstapi',
